d28b314741f505302e25c51faabbb419957d0960
Marco Molica <marco.molica@nigithorconsulting.com>
2022-04-05 23:21:11 +0200

    First try unity

--- src/c/ex1/quick_sort.c
+++ src/c/ex1/quick_sort.c
@@ -1,1 +1,4 @@
 #include "headers/quick_sort.h"
+
+
+
--- src/c/test/quick_sort_test.c
+++ src/c/test/quick_sort_test.c
@@ -1,8 +1,23 @@
 #include "unity/unity.h"
 #include "../ex1/headers/quick_sort.h"
 
+void setUp(void) {
+    printf("Hello World!");
+}
+
+void tearDown(void) {
+    printf("Hello World! 33333");
+}
+
+void abdullah(void) {
+    printf("Hello World! 44444");
+}
+
 int main(int argc, char const *argv[])
 {
     UNITY_BEGIN();
+    printf("Hello World! 222222");
+    RUN_TEST(abdullah);
+    RUN_TEST(abdullah);
     return UNITY_END();
 }
