2a83b718aa43017805035e5020905bc65ebb7478
eduard <occhipinti.eduard@icloud.com>
2022-04-05 11:16:48 +0200

    base structure

---
+++ src/c/ex1/quick_sort.c
@@ -0,0 +1,1 @@
+#include "headers/quick_sort.h"
---
+++ src/c/test/quick_sort_test.c
@@ -0,0 +1,8 @@
+#include "unity/unity.h"
+#include "../ex1/headers/quick_sort.h"
+
+int main(int argc, char const *argv[])
+{
+    UNITY_BEGIN();
+    return UNITY_END();
+}
