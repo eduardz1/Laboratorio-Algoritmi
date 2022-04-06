10559f273821827d877fb21b20e94f8411cb1f69
iman <iman.solaih@edu.unito.it>
2022-04-06 20:06:08 +0200

    header comparable and record

--- src/c/shared/common.h
+++ src/c/shared/common.h
@@ -1,1 +1,50 @@
 #pragma once
+
+
+
+/**
+
+ * @brief compare two integers
+ * 
+ * @param a first element of comparison  
+ * @param b second element of comparison
+ * @return -1 if a<b, 0 if a=b, 1 if a>b
+ */
+int compare_int(int a, int b);
+
+/**
+ * @brief compare two longs
+ * 
+ * @param a first element of comparison  
+ * @param b second element of comparison
+ * @return -1 if a<b, 0 if a=b, 1 if a>b
+ */
+int compare_long(long a, long b);
+
+/**
+ * @brief compare two floats
+ * 
+ * @param a first element of comparison  
+ * @param b second element of comparison
+ * @return -1 if a<b, 0 if a=b, 1 if a>b
+ */
+int compare_float(float a, float b);
+
+/**
+ * @brief compare two doubles
+ * 
+ * @param a first element of comparison  
+ * @param b second element of comparison
+ * @return -1 if a<b, 0 if a=b, 1 if a>b
+ */
+int compare_double(double a, double b);
+
+/**
+ * @brief compare two strings
+ * 
+ * @param a first element of comparison  
+ * @param b second element of comparison
+ * @return -1 if a<b, 0 if a=b, 1 if a>b
+ */
+int compare_string(char *a, char *b);
+
---
+++ src/c/shared/record.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "common.h"
+
+struct _record
+{
+  int id;
+  char *field1;
+  int  field2;
+  double field3;
+};
+
+/**
+ * @brief compare two records
+ * @details 
+ *  calls #compare_string on the first field, if the two strings are equal, 
+ *  calls #compare_int on the second field, if the two integers are equal,
+ *  calls #compare_double on the third field
+ * @param a first element of comparison  
+ * @param b second element of comparison
+ * @return -1 if a<b, 0 if a=b, 1 if a>b
+ * 
+ */
+int compare_records( _record a, _record b);
