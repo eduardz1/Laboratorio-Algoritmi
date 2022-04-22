cd4952e8fe44c0f24f399398466d9eb48a496e32
eduard <occhipinti.eduard@icloud.com>
2022-04-22 17:06:58 +0200

    r script for sklist log refined

--- log_sklist.r
+++ log_sklist.r
@@ -1,22 +1,107 @@
-data.frame(time_log_skiplist)
-
-plot(time_log_skiplist$MAX_HEIGHT, time_log_skiplist$INSERT, col = "blue")
-points(time_log_skiplist$MAX_LEVEL, time_log_skiplist$INSERT, col = "red")
-
-plot(time_log_skiplist$MAX_HEIGHT, time_log_skiplist$INSERT, xlim = c(15,39), ylim = c(0.96, 1.04), col = "blue")
-points(time_log_skiplist$MAX_LEVEL, time_log_skiplist$INSERT, xlim = c(15,39), ylim = c(0.96, 1.04), col = "red")
-
-plot(time_log_skiplist$MAX_HEIGHT, time_log_skiplist$SEARCH, col = "blue")
-points(time_log_skiplist$MAX_LEVEL, time_log_skiplist$SEARCH, col = "red")
-
-plot(time_log_skiplist$MAX_HEIGHT, time_log_skiplist$SEARCH, xlim = c(15,39), ylim = c(5e-05, 2e-04), col = "blue")
-points(time_log_skiplist$MAX_LEVEL, time_log_skiplist$SEARCH, xlim = c(15,39), ylim = c(5e-05, 2e-04), col = "red")
-
+log <- data.frame(time_log_skiplist)
 
 getmode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
-getmode(time_log_skiplist$MAX_LEVEL) # 20
-summary(time_log_skiplist)
+getmode(log$MAX_LEVEL) # 20
+summary(log)
+
+# Insert #######################################################################
+plot(log$MAX_HEIGHT,
+     log$INSERT,
+     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+     xlab = "Levels",
+     ylab = "Time (s)",
+     main = "Skip List Insert")
+points(log$MAX_LEVEL,
+       log$INSERT,
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
+legend(30, 
+       3.2,
+       legend = c("Max level reached", "Max level reachable"),
+       fill = c("red", "blue"))
+## Zoom in on insert ##
+plot(log$MAX_HEIGHT,
+     log$INSERT,
+     ylim = c(0.96, 1.04),
+     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+     xlab = "Levels",
+     ylab = "Time (s)",
+     main = "Skip List Insert")
+points(log$MAX_LEVEL,
+       log$INSERT,
+       ylim = c(0.96, 1.04),
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
+legend(30, 
+       1.035,
+       legend = c("Max level reached", "Max level reachable"),
+       fill = c("red", "blue"))
+## Mean time of insert ##
+levels <- c(15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)
+mean_time_insert <- c(mean(log$INSERT[log$MAX_LEVEL == 15]),
+                      mean(log$INSERT[log$MAX_LEVEL == 16]),
+                      mean(log$INSERT[log$MAX_LEVEL == 17]),
+                      mean(log$INSERT[log$MAX_LEVEL == 18]),
+                      mean(log$INSERT[log$MAX_LEVEL == 19]),
+                      mean(log$INSERT[log$MAX_LEVEL == 20]),
+                      mean(log$INSERT[log$MAX_LEVEL == 21]),
+                      mean(log$INSERT[log$MAX_LEVEL == 22]),
+                      mean(log$INSERT[log$MAX_LEVEL == 23]),
+                      mean(log$INSERT[log$MAX_LEVEL == 24]),
+                      mean(log$INSERT[log$MAX_LEVEL == 25]))
+plot(levels,
+     mean_time_insert,
+     pch = 16,
+     cex = 2,
+     col = rgb(red = 0.8, green = 0, blue = 0.1, alpha = 0.5))
+################################################################################
+## Search ######################################################################
+plot(log$MAX_HEIGHT,
+     log$SEARCH,
+     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+     xlab = "Levels",
+     ylab = "Time (s)",
+     main = "Skip List Search")
+points(log$MAX_LEVEL,
+       log$SEARCH,
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
+legend(30, 
+       4e-04,
+       legend = c("Max level reached", "Max level reachable"),
+       fill = c("red", "blue"))
+## Zoom in on search ##
+plot(log$MAX_HEIGHT, 
+     log$SEARCH,  
+     ylim = c(5e-05, 2e-04), 
+     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+     xlab = "Levels",
+     ylab = "Time (s)",
+     main = "Skip List Search")
+points(log$MAX_LEVEL,
+       log$SEARCH, 
+       ylim = c(5e-05, 2e-04), 
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
+legend(30, 
+       0.00019,
+       legend = c("Max level reached", "Max level reachable"),
+       fill = c("red", "blue"))
+## Mean time of search ##
+mean_time_search <- c(mean(log$SEARCH[log$MAX_LEVEL == 15]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 16]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 17]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 18]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 19]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 20]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 21]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 22]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 23]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 24]),
+                      mean(log$SEARCH[log$MAX_LEVEL == 25]))
+plot(levels,
+     mean_time_search,
+     pch = 16,
+     cex = 2,
+     col = rgb(red = 0.8, green = 0, blue = 0.1, alpha = 0.5))
+################################################################################
