44700c9665155718403d1a85b2c505b6279023b3
eduard <occhipinti.eduard@icloud.com>
2022-04-25 18:06:50 +0200

    refinements

--- log_sklist.r
+++ log_sklist.r
@@ -1,8 +1,12 @@
-log <- data.frame(time_log_skiplist)
+library(data.table)
+log <- data.frame(fread(
+       file = "time_log_skiplist.csv",
+       sep = ";", header = T, na.strings = "NA"
+))
 
 getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
+       uniqv <- unique(v)
+       uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
 getmode(log$MAX_LEVEL) # 20
@@ -10,98 +14,116 @@ summary(log)
 
 # Insert #######################################################################
 plot(log$MAX_HEIGHT,
-     log$INSERT,
-     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
-     xlab = "Levels",
-     ylab = "Time (s)",
-     main = "Skip List Insert")
+       log$INSERT,
+       col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+       xlab = "Levels",
+       ylab = "Time (s)",
+       main = "Skip List Insert"
+)
 points(log$MAX_LEVEL,
        log$INSERT,
-       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
-legend(30, 
-       3.2,
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5)
+)
+legend(30,
+       110,
        legend = c("Max level reached", "Max level reachable"),
-       fill = c("red", "blue"))
+       fill = c("red", "blue")
+)
 ## Zoom in on insert ##
 plot(log$MAX_HEIGHT,
-     log$INSERT,
-     ylim = c(0.96, 1.04),
-     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
-     xlab = "Levels",
-     ylab = "Time (s)",
-     main = "Skip List Insert")
+       log$INSERT,
+       ylim = c(0.96, 1.04),
+       col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+       xlab = "Levels",
+       ylab = "Time (s)",
+       main = "Skip List Insert"
+)
 points(log$MAX_LEVEL,
        log$INSERT,
        ylim = c(0.96, 1.04),
-       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
-legend(30, 
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5)
+)
+legend(30,
        1.035,
        legend = c("Max level reached", "Max level reachable"),
-       fill = c("red", "blue"))
+       fill = c("red", "blue")
+)
 ## Mean time of insert ##
 levels <- c(15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)
-mean_time_insert <- c(mean(log$INSERT[log$MAX_LEVEL == 15]),
-                      mean(log$INSERT[log$MAX_LEVEL == 16]),
-                      mean(log$INSERT[log$MAX_LEVEL == 17]),
-                      mean(log$INSERT[log$MAX_LEVEL == 18]),
-                      mean(log$INSERT[log$MAX_LEVEL == 19]),
-                      mean(log$INSERT[log$MAX_LEVEL == 20]),
-                      mean(log$INSERT[log$MAX_LEVEL == 21]),
-                      mean(log$INSERT[log$MAX_LEVEL == 22]),
-                      mean(log$INSERT[log$MAX_LEVEL == 23]),
-                      mean(log$INSERT[log$MAX_LEVEL == 24]),
-                      mean(log$INSERT[log$MAX_LEVEL == 25]))
+mean_time_insert <- c(
+       mean(log$INSERT[log$MAX_LEVEL == 15]),
+       mean(log$INSERT[log$MAX_LEVEL == 16]),
+       mean(log$INSERT[log$MAX_LEVEL == 17]),
+       mean(log$INSERT[log$MAX_LEVEL == 18]),
+       mean(log$INSERT[log$MAX_LEVEL == 19]),
+       mean(log$INSERT[log$MAX_LEVEL == 20]),
+       mean(log$INSERT[log$MAX_LEVEL == 21]),
+       mean(log$INSERT[log$MAX_LEVEL == 22]),
+       mean(log$INSERT[log$MAX_LEVEL == 23]),
+       mean(log$INSERT[log$MAX_LEVEL == 24]),
+       mean(log$INSERT[log$MAX_LEVEL == 25])
+)
 plot(levels,
-     mean_time_insert,
-     pch = 16,
-     cex = 2,
-     col = rgb(red = 0.8, green = 0, blue = 0.1, alpha = 0.5))
+       mean_time_insert,
+       pch = 16,
+       cex = 2,
+       col = rgb(red = 0.8, green = 0, blue = 0.1, alpha = 0.5)
+)
 ################################################################################
 ## Search ######################################################################
 plot(log$MAX_HEIGHT,
-     log$SEARCH,
-     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
-     xlab = "Levels",
-     ylab = "Time (s)",
-     main = "Skip List Search")
+       log$SEARCH,
+       col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+       xlab = "Levels",
+       ylab = "Time (s)",
+       main = "Skip List Search"
+)
 points(log$MAX_LEVEL,
        log$SEARCH,
-       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
-legend(30, 
-       4e-04,
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5)
+)
+legend(30,
+       0.016,
        legend = c("Max level reached", "Max level reachable"),
-       fill = c("red", "blue"))
+       fill = c("red", "blue")
+)
 ## Zoom in on search ##
-plot(log$MAX_HEIGHT, 
-     log$SEARCH,  
-     ylim = c(5e-05, 2e-04), 
-     col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
-     xlab = "Levels",
-     ylab = "Time (s)",
-     main = "Skip List Search")
+plot(log$MAX_HEIGHT,
+       log$SEARCH,
+       ylim = c(5e-05, 2e-04),
+       col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5),
+       xlab = "Levels",
+       ylab = "Time (s)",
+       main = "Skip List Search"
+)
 points(log$MAX_LEVEL,
-       log$SEARCH, 
-       ylim = c(5e-05, 2e-04), 
-       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5))
-legend(30, 
+       log$SEARCH,
+       ylim = c(5e-05, 2e-04),
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.5)
+)
+legend(30,
        0.00019,
        legend = c("Max level reached", "Max level reachable"),
-       fill = c("red", "blue"))
+       fill = c("red", "blue")
+)
 ## Mean time of search ##
-mean_time_search <- c(mean(log$SEARCH[log$MAX_LEVEL == 15]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 16]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 17]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 18]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 19]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 20]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 21]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 22]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 23]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 24]),
-                      mean(log$SEARCH[log$MAX_LEVEL == 25]))
+mean_time_search <- c(
+       mean(log$SEARCH[log$MAX_LEVEL == 15]),
+       mean(log$SEARCH[log$MAX_LEVEL == 16]),
+       mean(log$SEARCH[log$MAX_LEVEL == 17]),
+       mean(log$SEARCH[log$MAX_LEVEL == 18]),
+       mean(log$SEARCH[log$MAX_LEVEL == 19]),
+       mean(log$SEARCH[log$MAX_LEVEL == 20]),
+       mean(log$SEARCH[log$MAX_LEVEL == 21]),
+       mean(log$SEARCH[log$MAX_LEVEL == 22]),
+       mean(log$SEARCH[log$MAX_LEVEL == 23]),
+       mean(log$SEARCH[log$MAX_LEVEL == 24]),
+       mean(log$SEARCH[log$MAX_LEVEL == 25])
+)
 plot(levels,
-     mean_time_search,
-     pch = 16,
-     cex = 2,
-     col = rgb(red = 0.8, green = 0, blue = 0.1, alpha = 0.5))
+       mean_time_search,
+       pch = 16,
+       cex = 2,
+       col = rgb(red = 0.8, green = 0, blue = 0.1, alpha = 0.5)
+)
 ################################################################################
--- log_sort.r
+++ log_sort.r
@@ -1,10 +1,61 @@
-data.frame(time_log_qsort)
+library(ggplot2)
+library(Rmisc)
+library(data.table)
+fp <- data.frame(fread(
+       file = "time_log_qsort.csv",
+       sep = ";", header = T, na.strings = "NA"
+))
 
-boxplot(time_log_qsort$MEDIAN3,
-        time_log_qsort$RANDOM,
-        time_log_qsort$FIRST,
-        time_log_qsort$MIDDLE,
-        time_log_qsort$LAST,
-        horizontal = FALSE, outline = FALSE, names = c("MEDIAN3", "RANDOM", "FIRST", "MIDDLE", "LAST"))
+run_20kk <- data.frame(fp[fp$size == 20000000, ])
 
-summary(time_log_qsort$MEDIAN3)
+boxplot(run_20kk$MEDIAN3,
+       run_20kk$RANDOM,
+       run_20kk$FIRST,
+       run_20kk$MIDDLE,
+       run_20kk$LAST,
+       horizontal = FALSE,
+       outline = FALSE,
+       names = c("MEDIAN3", "RANDOM", "FIRST", "MIDDLE", "LAST")
+)
+
+summary(run_20kk$MEDIAN3)
+################################################################################
+################################################################################
+pivot <- data.frame(fp[fp$size != 20000000, ])
+pivot <- na.omit(pivot)
+## Exponential graph ##
+plot(pivot$size,
+       pivot$FIRST,
+       type = "l",
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 1)
+)
+lines(pivot$size,
+       pivot$RANDOM,
+       col = rgb(red = 0, green = 0, blue = 1, alpha = 1)
+)
+lines(pivot$size,
+       pivot$MEDIAN3,
+       col = rgb(red = 0, green = 1, blue = 0, alpha = 1)
+)
+lines(pivot$size,
+       pivot$MIDDLE,
+       col = rgb(red = 0.5, green = 0, blue = 0.5, alpha = 1)
+)
+lines(pivot$size,
+       pivot$LAST,
+       col = rgb(red = 0.5, green = 0.5, blue = 0, alpha = 1)
+)
+## Zoom in on the pivot that don't degenerate in O(n^2) ##
+pivot <- data.frame(fp[fp$size != 20000000, ])
+pivot <- pivot[, c("MEDIAN3", "MIDDLE", "RANDOM", "size")];
+
+ggplot(pivot, aes(pivot$size, pivot$MEDIAN3)) +
+       geom_smooth(aes(pivot$size, pivot$MEDIAN3, group = 1),
+              method = "loess", color = "#ff6c29"
+       ) +
+       geom_smooth(aes(pivot$size, pivot$MIDDLE, group = 1),
+              method = "loess", color = "#84da33"
+       ) +
+       geom_smooth(aes(pivot$size, pivot$RANDOM, group = 1),
+              method = "loess", color = "#2558ff"
+       )
