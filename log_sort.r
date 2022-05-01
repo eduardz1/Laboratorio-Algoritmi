fdfb8199ef1c06103f8b1b12b70e3cd05e1d13d4
eduard <occhipinti.eduard@icloud.com>
2022-05-01 20:02:58 +0200

    loess smoothing is more accurate

--- log_sort.r
+++ log_sort.r
@@ -10,7 +10,7 @@ tikz("latex/figures/qs_boxplot_pivot.tex", width = 5, height = 3)
 
 # section only containing run on 20 million shuffled records
 main <- data.frame(pivot = fp[fp$size == 20000000 & fp$file == "es1_dataset/records.csv", ]$pivot,
-                       time = fp[fp$size == 20000000 & fp$file == "es1_dataset/records.csv", ]$time)
+                   time = fp[fp$size == 20000000 & fp$file == "es1_dataset/records.csv", ]$time)
 
 ggplot(main, aes(x = pivot, y = time)) +
        geom_boxplot() +
@@ -27,7 +27,7 @@ pivot <- data.frame(time = fp[fp$file == "es1_dataset/sorted.csv", ]$time,
                     size = fp[fp$file == "es1_dataset/sorted.csv", ]$size)
 
 ggplot(pivot, aes(x = size, y = time, color = pivot)) + 
-       geom_smooth() + ylab("time (s)") + theme_bw()
+       geom_smooth(method = loess, se = FALSE) + ylab("time (s)") + theme_bw()
 dev.off()
 ## Zoom in on the pivot that don't degenerate in O(n^2) ##
 tikz("latex/figures/qs_plot_zoommed_pivot.tex", width = 5, height = 3)
@@ -36,7 +36,7 @@ pivot_sec <- pivot[pivot$pivot == "MEDIAN3" |
                    pivot$pivot == "MIDDLE", ]
 
 ggplot(pivot_sec, aes(x = size, y = time, color = pivot)) + 
-       geom_smooth() + ylab("time (s)") + theme_bw()
+       geom_smooth(method = loess, se = FALSE) + ylab("time (s)") + theme_bw()
 dev.off()
 ################################################################################
 ## BINARY INSERTION SORT #######################################################
@@ -49,5 +49,5 @@ bissort <- data.frame(time = fpp[fpp$file == "es1_dataset/records.csv", ]$time,
                       size = fpp[fpp$file == "es1_dataset/records.csv", ]$size)
 
 ggplot(bissort, aes(x = size, y = time)) + 
-       geom_smooth() + ylab("time (s)") + theme_bw()
+       geom_smooth(method = loess, se = FALSE) + ylab("time (s)") + theme_bw()
 dev.off()
