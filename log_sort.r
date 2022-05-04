2ed48c0802cf85e08fec1d2d3e6260dfdaf4068a
eduard <occhipinti.eduard@icloud.com>
2022-05-04 14:52:06 +0200

    isolating the analysis on only compare_records_string

--- log_sort.r
+++ log_sort.r
@@ -33,12 +33,15 @@ summary(main$time[main$pivot == "MEDIAN3"])
 ## SORTED ######################################################################
 tikz("latex/figures/qs_plot_pivot.tex", width = 5, height = 2)
 
-pivot <- data.frame(time = fp[fp$file == "es1_dataset/sorted.csv", ]$time,
-                    pivot = fp[fp$file == "es1_dataset/sorted.csv", ]$pivot,
-                    size = fp[fp$file == "es1_dataset/sorted.csv", ]$size)
+pivot <- data.frame(time = fp[fp$file == "es1_dataset/sorted.csv" &
+                              fp$compare == "records_string", ]$time,
+                    pivot = fp[fp$file == "es1_dataset/sorted.csv" &
+                               fp$compare == "records_string", ]$pivot,
+                    size = fp[fp$file == "es1_dataset/sorted.csv" &
+                              fp$compare == "records_string", ]$size)
 
 ggplot(pivot, aes(x = size, y = time, color = pivot)) + 
-       geom_smooth(method = loess, se = FALSE) + 
+       geom_line() + 
        ylab("time (s)") + 
        theme_bw()
 dev.off()
@@ -49,7 +52,7 @@ pivot_sec <- pivot[pivot$pivot == "MEDIAN3" |
                    pivot$pivot == "MIDDLE", ]
 
 ggplot(pivot_sec, aes(x = size, y = time, color = pivot)) + 
-       geom_smooth(method = loess, se = FALSE) + 
+       geom_line() + 
        ylab("time (s)") + 
        theme_bw()
 dev.off()
@@ -60,12 +63,15 @@ fpp <- data.frame(fread(
        file = "time_log_insertsort.csv",
        sep = ";", header = T, na.strings = "NA"
 ))
-bissort <- data.frame(time = fpp[fpp$file == "es1_dataset/records.csv" & fpp$size, ]$time,
-                      size = fpp[fpp$file == "es1_dataset/records.csv" & fpp$size, ]$size,
-                      binarySearch = fpp[fpp$file == "es1_dataset/records.csv" & fpp$size, ]$binary_search)
+bissort <- data.frame(time = fpp[fpp$file == "es1_dataset/records.csv" &
+                                 fpp$compare == "records_string", ]$time,
+                      size = fpp[fpp$file == "es1_dataset/records.csv" &
+                                 fpp$compare == "records_string", ]$size,
+                      binarySearch = fpp[fpp$file == "es1_dataset/records.csv" &
+                                         fpp$compare == "records_string", ]$binary_search)
 
 ggplot(bissort, aes(x = size, y = time, color = binarySearch)) + 
-       geom_smooth(method = loess, se = FALSE) + 
+       geom_line() + #geom_smooth(method = loess, se = FALSE) + 
        ylab("time (s)") + 
        theme_bw()
 dev.off()
