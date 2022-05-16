6e3789bf41048484d741cfad7281c77c49a7aef9
Marco Molica <marco.molica@edu.unito.it>
2022-05-17 00:44:08 +0200

    FIx main ex4

--- src/main/java/ex4/Main.java
+++ src/main/java/ex4/Main.java
@@ -13,9 +13,10 @@ import ex4.helpers.GraphHelper;
 public class Main {
   public static void main(String[] args) throws Exception {
     try {
-      File file = new File("/home/eduard/Github/laboratorio-algoritmi-2021-2022/es4_dataset/italian_dist_graph.csv");
+      // File file = new File("/home/eduard/Github/laboratorio-algoritmi-2021-2022/es4_dataset/italian_dist_graph.csv");
+      File file = new File("/home/emme/laboratorio-algoritmi-2021-2022/es4_dataset/italian_dist_graph.csv");
       Scanner scanner = new Scanner(file);
-      Graph<String, Float> graph = new Graph<>(true);
+      Graph<String, Float> graph = new Graph<>(false);
 
       while (scanner.hasNextLine()) {
         String line = scanner.nextLine();
@@ -25,6 +26,15 @@ public class Main {
         String[] tokens = line.split(",");
         Edge<String, Float> edge = new Edge<>(tokens[0], tokens[1], Float.parseFloat(tokens[2]));
         graph.addEdge(edge);
+
+        // if (!graph.containsVertex(tokens[0]))
+        //   graph.addVertex(tokens[0]);
+
+        // if (!graph.containsVertex(tokens[1]))
+        //   graph.addVertex(tokens[2]);
+
+        // graph.addEdge(tokens[0], tokens[1], Float.parseFloat(tokens[2]));
+  
       }
       scanner.close();
 
@@ -35,6 +45,10 @@ public class Main {
           Float.MAX_VALUE,
           "torino", 
           "catania");
+
+      for (String string : res.getFirst()) {
+        System.out.println(string);
+      }
 
       System.out.println(res.getSecond());
     } catch (FileNotFoundException e) {
